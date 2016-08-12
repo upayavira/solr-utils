b557b651c69203b4c922e796fd4266c299ecc9f2
Upayavira <uv@odoko.co.uk>
2016-08-13 01:13:50 +0200

    Fix registry naming

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ REGISTRY=${REGISTRY/-}
 mvn package
 docker build -t ${REGISTRY}solr:${VERSION} .
 if [ "$1" = "push" ]; then
-  docker push docker.odoko.org/solr:${VERSION}
+  docker push ${REGISTRY}solr:${VERSION}
 fi
