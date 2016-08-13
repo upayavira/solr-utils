377d419868cf688ea6998462893645327a9b05bc
Upayavira <uv@odoko.co.uk>
2016-08-13 22:07:52 +0200

    State what you will do

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@
 VERSION=${VERSION-latest}
 REGISTRY=${REGISTRY/-}
 
+echo "Using registry $REGISTRY"
 mvn package
 docker build -t ${REGISTRY}solr:${VERSION} .
 if [ "$1" = "push" ]; then
+  echo "Pushing..."
   docker push ${REGISTRY}solr:${VERSION}
 fi
